0d052078e15bf1af904a0ca997157222a01b1b26
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-01-31 19:46:35 +0530

    stopped because of google asking for billing

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,31 @@
 import React from "react";
-// import useStyle from "./styles";
+import useStyles from "./styles";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
 
 const Map = () => {
-    // const classes = useStyle();
+    const classes = useStyles();
+    const isMobile = useMediaQuery("(min-width:600px)");
+
+    const coordinates = { lat: 0, lng: 0 };
 
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: "AIzaSyDHXXWB8X2KA7WC8jZpiCKjCYy1Ng_dCcA" }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={""}
+                onChange={""}
+                onChildClick={""}
+            >
+
+            </GoogleMapReact>
+        </div>
     );
 }
 
